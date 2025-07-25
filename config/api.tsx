65fd23f5d1d5d32fadb59b82412418600d04d27e
Jonas Feragen <jonas.feragen@gmail.com>
2025-07-25 10:41:23 +0200

    WIP

--- config/api.tsx
+++ config/api.tsx
@@ -1,13 +1,18 @@
 // Base URL for API requests
+import { Platform } from 'react-native';
+
 export const API_BASE_URL = __DEV__
-  ? 'http://10.0.2.2:3001'  // For Android emulator
+  ? Platform.select({
+    android: 'http://10.0.2.2:3001', // Android emulator
+    ios: 'http://localhost:3001',   // iOS simulator
+    web: 'http://localhost:3001',   // Web browser
+  })!
   : 'https://your-production-api.com'; // Update this for production
 
 export const PAGE_SIZE = 3;
 
 const CATEGORIES_URL = `${API_BASE_URL}/products/categories`;
 const PRODUCTS_URL = `${API_BASE_URL}/products`;
-const TAGS_URL = `${API_BASE_URL}/products/tags`;
 const filterParams = `status=publish&per_page=${PAGE_SIZE}&hide_empty=true`;
 
 // API endpoints
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,40 +3,14 @@ import { ProductTiles } from '@/components/features/product/ProductTiles';
 import { PageContent, PageSection, PageView } from '@/components/layout';
 import { PageHeader } from '@/components/layout/PageHeader';
 
-import { CategoryTile } from '@/components/features/category/CategoryTile';
 import { SearchBar } from '@/components/ui/searchBar/Searchbar';
-import { ThemedSpinner } from '@/components/ui/ThemedSpinner';
 import { routes } from '@/config/routes';
 import { useCategories } from '@/hooks/data/Category';
 import { useDiscountedProducts, useFeaturedProducts, useProductsByIds, useRecentProducts } from '@/hooks/data/Product';
 import { useRunOnFocus } from '@/hooks/useRunOnFocus';
 import { router, Stack } from 'expo-router';
 import { TextInput } from 'react-native';
-import { XStack } from 'tamagui';
 
-const CategorySection = () => {
-    const { items: categories, isLoading } = useCategories(0, { autoload: true });
-
-    if (isLoading) {
-        return <ThemedSpinner ai="center" jc="center" size="large" />;
-    }
-    return (
-        <XStack flexWrap="wrap" gap="$4" jc="space-between">
-            {categories.map((category) => (
-                <CategoryTile
-                    key={category.id.toString()}
-                    category={category}
-                    style={{
-                        aspectRatio: 1,
-                        flexBasis: '30%',
-                        flexGrow: 1,
-                    }}
-                    theme={'primary'}
-                />
-            ))}
-        </XStack>
-    );
-};
 
 export const HomeScreen = () => {
     const searchInputRef = useRunOnFocus<TextInput>((input) => input.focus());
@@ -58,9 +32,6 @@ export const HomeScreen = () => {
                 <SearchBar ref={searchInputRef} initialQuery="" onSubmit={handleSearch} />
             </PageHeader>
             <PageSection scrollable>
-                <PageContent title="Kategorier">
-                    <CategoryTiles key='categories' queryResult={categories} theme="primary" />
-                </PageContent>
                 <PageContent title="Debug">
                     <ProductTiles key='debug' queryResult={debugProducts} theme="secondary" />
                 </PageContent>
@@ -69,6 +40,9 @@ export const HomeScreen = () => {
                 </PageContent>
                 <PageContent theme="primary" title="Tilbud">
                     <ProductTiles key='discounted' queryResult={discountedProducts} theme="secondary" />
+                </PageContent>
+                <PageContent title="Kategorier">
+                    <CategoryTiles key='categories' queryResult={categories} theme="primary" />
                 </PageContent>
                 <PageContent theme="primary" title="Populære produkter" >
                     <ProductTiles key='featured' queryResult={featuredProducts} theme="secondary" />
