47ec75ece08da3271b0251a1a6b5f0c1e0b7d57b
Jonas Feragen <jonas.feragen@gmail.com>
2025-07-28 21:04:55 +0200

    WIP

--- components/menu/BottomBar.tsx
+++ components/menu/BottomBar.tsx
@@ -1,67 +1,83 @@
 import { LinearGradient } from '@tamagui/linear-gradient';
 import { Home, Search, ShoppingCart } from '@tamagui/lucide-icons';
 import { Link } from 'expo-router';
-import { Tabs, Text, Theme, YStack } from 'tamagui';
+import { styled, Tabs, Text, Theme, YStack } from 'tamagui';
+
+const StyledTab = styled(Tabs.Tab, {
+    name: 'StyledTab',
+    padding: '$2',
+    flex: 1,
+    position: 'relative',
+    bottom: 50,
+    height: 100,
+    left: 0,
+    right: 0,
+});
+
+const StyledLinearGradient = styled(LinearGradient, {
+    name: 'StyledLinearGradient',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+});
+
+const StyledTabs = styled(Tabs, {
+    name: 'StyledTabs',
+    height: 110,
+    backgroundColor: 'transparent',
+    borderTopColor: '$borderColor',
+    borderTopWidth: 1,
+    position: 'relative',
+    bottom: -40,
+    left: 0,
+    right: 0,
+});
+
+const StyledTabsList = styled(Tabs.List, {
+    name: 'StyledTabsList',
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+});
 
 export const BottomBar = ({ children }: { children?: React.ReactNode }) => {
-
-
-    const tabStyle = {
-        padding: '$2',
-        flex: 1,
-        position: 'relative',
-        bottom: 50,
-        height: 100,
-        left: 0,
-        right: 0
-    };
-
     return (
         <Theme name="secondary">
-            <LinearGradient
+            <StyledLinearGradient
                 colors={['$background', '$backgroundPress']}
                 start={[0, 0]}
                 end={[1, 0]}
-                style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
             />
-            <Tabs
-
-                height={110}
-                backgroundColor="transparent"
-                borderTopColor="$borderColor"
-                borderTopWidth={1}
-                position="relative"
-                bottom={-40}
-                left={0}
-                right={0}
-            >
-                <Tabs.List width="100%" position="absolute" bottom={0}>
-                    <Tabs.Tab style={tabStyle} value="home">
+            <StyledTabs>
+                <StyledTabsList>
+                    <StyledTab value="home">
                         <Link href="/">
                             <YStack ai="center" >
                                 <Home />
                                 <Text>Hjem</Text>
                             </YStack>
                         </Link>
-                    </Tabs.Tab>
-                    <Tabs.Tab style={tabStyle} value="search">
+                    </StyledTab>
+                    <StyledTab value="search">
                         <Link href="/search">
                             <YStack ai="center">
                                 <Search />
                                 <Text>Søk</Text>
                             </YStack>
                         </Link>
-                    </Tabs.Tab>
-                    <Tabs.Tab style={tabStyle} value="cart">
+                    </StyledTab>
+                    <StyledTab value="cart">
                         <Link href="/shopping-cart">
                             <YStack ai="center">
                                 <ShoppingCart />
                                 <Text>Handlekurv</Text>
                             </YStack>
                         </Link>
-                    </Tabs.Tab>
-                </Tabs.List>
-            </Tabs>
+                    </StyledTab>
+                </StyledTabsList>
+            </StyledTabs>
         </Theme>
 
     );
