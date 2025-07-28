53454c9d33b7ef0392d5e976d9832ae0ce15edda
Jonas Feragen <jonas.feragen@gmail.com>
2025-07-28 08:59:01 +0200

    WIP

--- components/menu/SideBar.tsx
+++ components/menu/SideBar.tsx
@@ -1,33 +1,43 @@
 import { CustomHeader } from "@/components/menu/CustomHeader";
+import { DrawerContentComponentProps, DrawerHeaderProps } from "@react-navigation/drawer";
 import Drawer from "expo-router/drawer";
+import React, { JSX } from "react";
 import { Theme, useTheme } from "tamagui";
 import { CustomDrawerContent } from "./CustomDrawerContent";
 
-export const SideBar = ({ children }: { children: React.ReactNode }) =>
+export const SideBar = ({ children }: { children: React.ReactNode }): JSX.Element =>
     <Theme name="primary">
         <SideBarContent children={children} />
     </Theme>
 
 
-const SideBarContent = ({ children }: { children: React.ReactNode }) => {
+const SideBarContent = ({ children }: { children: React.ReactNode }): JSX.Element => {
     const theme = useTheme();
+
+    const drawerContent = React.useCallback(
+        (props: DrawerContentComponentProps) => <CustomDrawerContent {...props} />,
+        []
+    );
+
+    const screenOptions = React.useMemo(() => ({
+        drawerStyle: {
+            backgroundColor: theme.background.val,
+            borderColor: theme.borderColor.val,
+            borderWidth: 1,
+        },
+        drawerActiveBackgroundColor: theme.backgroundFocus.val,
+        drawerActiveTintColor: theme.color.val,
+        drawerInactiveTintColor: theme.color10.val,
+        drawerLabelStyle: {
+            fontSize: 16,
+        },
+        headerShown: true,
+        header: (props: DrawerHeaderProps) => <CustomHeader {...props} />,
+    }), [theme]);
+
     return <Drawer
-        drawerContent={(props) => <CustomDrawerContent {...props} />}
-        screenOptions={{
-            drawerStyle: {
-                backgroundColor: theme.background.val,
-                borderColor: theme.borderColor.val,
-                borderWidth: 1,
-            },
-            drawerActiveBackgroundColor: theme.backgroundFocus.val,
-            drawerActiveTintColor: theme.color.val,
-            drawerInactiveTintColor: theme.color10.val,
-            drawerLabelStyle: {
-                fontSize: 16,
-            },
-            headerShown: true,
-            header: (props) => <CustomHeader {...props} />,
-        }}
+        drawerContent={drawerContent}
+        screenOptions={screenOptions}
     >
         <Drawer.Screen
             name="index" // This is the name of the page and must match the url from root
