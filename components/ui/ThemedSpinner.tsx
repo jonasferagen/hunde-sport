8620bdd71f2ae581d1adb911291771bd3184c460
Jonas Feragen <jonas.feragen@gmail.com>
2025-07-30 14:01:05 +0200

    WIP

--- components/ui/ThemedSpinner.tsx
+++ components/ui/ThemedSpinner.tsx
@@ -3,5 +3,4 @@ import { Spinner, styled } from 'tamagui';
 export const ThemedSpinner = styled(Spinner, {
     name: 'ThemedSpinner',
     color: '$color',
-    marginVertical: '$4',
 });
--- components/ui/tile/HorizontalTiles.tsx
+++ components/ui/tile/HorizontalTiles.tsx
@@ -1,7 +1,7 @@
 import { ThemedSpinner } from '@/components/ui/ThemedSpinner';
 import { InfiniteListQueryResult } from '@/hooks/data/util';
 import { FlashList, ListRenderItem } from '@shopify/flash-list';
-import { ChevronRight } from '@tamagui/lucide-icons';
+import { ChevronLeft, ChevronRight } from '@tamagui/lucide-icons';
 import React, { JSX, useState } from 'react';
 import { NativeScrollEvent, NativeSyntheticEvent } from 'react-native';
 import { View } from 'tamagui';
@@ -12,26 +12,30 @@ interface HorizontalTilesProps<T> {
     renderItem: ListRenderItem<T>;
 }
 
+
 export const HorizontalTiles = <T extends { id: number | string }>({
     queryResult,
     renderItem,
 }: HorizontalTilesProps<T>): JSX.Element => {
     const { items, isFetchingNextPage, fetchNextPage } = queryResult;
-    const [showIndicator, setShowIndicator] = useState(true);
+    const [scrollOffset, setScrollOffset] = useState(0);
     const [contentWidth, setContentWidth] = useState(0);
     const [containerWidth, setContainerWidth] = useState(0);
 
     const isScrollable = contentWidth > containerWidth;
+    const isAtStart = scrollOffset <= 10;
+    const isAtEnd = scrollOffset + containerWidth >= contentWidth - 10;
 
     if (!items || items.length === 0) {
         return <></>;
     }
 
     const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
-        if (showIndicator && event.nativeEvent.contentOffset.x > 10) {
-            setShowIndicator(false);
-        }
+        setScrollOffset(event.nativeEvent.contentOffset.x);
     };
+
+    const SPACING = "$3";
+
 
     return (
         <View position="relative">
@@ -41,30 +45,28 @@ export const HorizontalTiles = <T extends { id: number | string }>({
                 renderItem={renderItem}
                 keyExtractor={(item) => item.id.toString()}
                 showsHorizontalScrollIndicator={false}
-                ItemSeparatorComponent={() => <View width="$2" />}
+                ItemSeparatorComponent={() => <View w={SPACING} />}
                 estimatedItemSize={100}
                 onScroll={handleScroll}
-                onContentSizeChange={(width) => {
-                    setContentWidth(width);
-                }}
-                onLayout={(e) => {
-                    setContainerWidth(e.nativeEvent.layout.width);
-                }}
+                scrollEventThrottle={16} // Trigger onScroll every 16ms
+                onContentSizeChange={setContentWidth}
+                onLayout={(e) => setContainerWidth(e.nativeEvent.layout.width)}
                 onEndReached={() => {
                     if (fetchNextPage) {
                         fetchNextPage();
                     }
                 }}
                 onEndReachedThreshold={0.5}
-                ListFooterComponent={isFetchingNextPage ? <ThemedSpinner /> : null}
+                ListHeaderComponent={<View w={SPACING} />}
+                ListFooterComponent={<View w={SPACING} flex={1} ai="center" jc="center">{isFetchingNextPage && <ThemedSpinner />}</View>}
             />
-            {isScrollable && showIndicator && (
+            {isScrollable && !isAtEnd && (
                 <View
                     position="absolute"
                     right={0}
                     top={0}
                     bottom={0}
-                    width={50}
+                    width={SPACING}
                     ai="center"
                     jc="center"
                     pe="none"
@@ -75,7 +77,29 @@ export const HorizontalTiles = <T extends { id: number | string }>({
                         end={[1, 0]}
                         style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0 }}
                     />
-                    <ChevronRight color="$color" size="$2" />
+                    <ChevronRight color="$color" size="$4" />
+                </View>
+            )}
+            {isScrollable && !isAtStart && (
+                <View
+                    position="absolute"
+                    left={0}
+                    top={0}
+                    bottom={0}
+                    width={SPACING}
+                    ai="center"
+                    jc="center"
+                    pe="none"
+                >
+                    <LinearGradient
+                        colors={['$backgroundTransparent', '$background']}
+                        start={[1, 0]}
+                        end={[0, 0]}
+                        style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0 }}
+                    />
+
+                    <ChevronLeft color="$color" size="$4" />
+
                 </View>
             )}
         </View>
